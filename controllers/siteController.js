8320d2e7c710705f1c8357db005c2c60cdb90526
ruvades <157583917+ruvades@users.noreply.github.com>
2025-06-02 16:28:27 +0200

    over en contact

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -3,9 +3,9 @@ exports.home = (req, res) => {
   res.render('home', { title: 'Sidewebz', stylesheet: 'home', year: currentYear, isHome: true });
 };
 
-exports.projects = (req, res) => {
+exports.over = (req, res) => {
   const currentYear = new Date().getFullYear();
-  res.render('projects', { title: 'Projecten', stylesheet: 'projects', year: currentYear });
+  res.render('over', { title: 'Over ons', stylesheet: 'over', year: currentYear });
 };
 
 exports.contact = (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const siteController = require('../controllers/siteController');
 
 router.get('/', siteController.home);
-router.get('/projects', siteController.projects);
+router.get('/over', siteController.over);
 router.get('/contact', siteController.contact);
 
 module.exports = router;
